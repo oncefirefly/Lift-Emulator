bc9d51e789ed43298b4cd7a96d75cfee58e80f17
oncefirefly <oncefirefly@gmail.com>
2023-01-08 20:29:45 +0300

    Call Lift button component added

---
+++ src/components/LiftEmolatorCallBtn.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  props: {
+    floor: {
+      type: Number,
+      required: true,
+      default: null,
+    },
+    lifts: {
+      type: Array,
+      required: false,
+      default: null,
+    },
+  },
+
+  computed: {
+    floorBtnStyle() {
+      return this.lifts.filter((lift) => lift.destFloor === this.floor).length
+        ? { background: "#FF6B6B" }
+        : { background: "#03a1fc" };
+    },
+  },
+};
+</script>
+
+<template>
+  <button
+    @click="(e) => this.$emit('callLift', e)"
+    class="call-btn"
+    :style="floorBtnStyle"
+  >
+    {{ floor }}
+  </button>
+</template>
+
+<style scoped>
+@import url(../assets/callBtn.css);
+</style>
