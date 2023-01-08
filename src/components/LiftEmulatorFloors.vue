570ed148196f3cc56e3e57f656f47ad37593c4f9
oncefirefly <oncefirefly@gmail.com>
2023-01-08 20:32:51 +0300

    moveLift method added + callLift updated

--- src/components/LiftEmulatorFloors.vue
+++ src/components/LiftEmulatorFloors.vue
@@ -1,4 +1,5 @@
 <script>
+import LiftEmolatorCallBtn from "./LiftEmolatorCallBtn.vue";
 import LiftEmulatorLift from "./LiftEmulatorLift.vue";
 
 export default {
@@ -17,10 +18,11 @@ export default {
 
   components: {
     LiftEmulatorLift,
+    LiftEmolatorCallBtn,
   },
 
   methods: {
-    getClosestEmptyLiftId(destFloor) {
+    getClosestEmptyLift(destFloor) {
       return this.lifts
         .filter((lift) => lift.busy !== true) // looking for empty not-busy lift
         .reduce((prev, curr) => {
@@ -29,20 +31,49 @@ export default {
             Math.abs(prev.currFloor - destFloor)
             ? curr
             : prev;
-        }).id;
+        });
     },
 
-    callLift(destFloor) {
+    moveLift(lift) {
+      setTimeout(() => {
+        lift.currFloor = lift.destFloor;
+        lift.resting = true;
+        lift.destFloor = -1;
+
+        setTimeout(() => {
+          lift.busy = false;
+          lift.resting = false;
+        }, 3000);
+      }, 1000 * lift.distance);
+    },
+
+    callLift(e) {
+      const destFloor = +e.target.innerHTML;
       // if there's any lift with the same current floor as the destination floor => do nothing
       if (this.lifts.filter((lift) => lift.currFloor === destFloor).length) {
-        console.log("I'm chillin");
         return;
       }
 
-      const closestLiftId = this.getClosestEmptyLiftId(destFloor);
+      const closestLift = this.getClosestEmptyLift(destFloor);
 
-      this.lifts.filter((lift) => lift.id === closestLiftId)[0].currFloor =
-        destFloor;
+      closestLift.destFloor = destFloor;
+
+      // prevents to call a new lift to existing destination floor
+      if (
+        this.lifts.filter(
+          (lift) =>
+            lift.destFloor === closestLift.destFloor &&
+            lift.id !== closestLift.id
+        ).length
+      ) {
+        return;
+      }
+
+      closestLift.distance = Math.abs(destFloor - closestLift.currFloor);
+
+      closestLift.busy = true;
+
+      this.moveLift(closestLift);
     },
   },
 };
@@ -51,49 +82,23 @@ export default {
 <template v-if="floors.length">
   <div class="floors">
     <div v-for="(floor, index) in floors" :key="index" class="floor">
-      <button @click="callLift(floor)" class="floor_btn">
-        {{ floor }}
-      </button>
+      <LiftEmolatorCallBtn
+        @call-lift="callLift"
+        :floor="floor"
+        :lifts="lifts"
+      />
       <div
         v-for="lift in lifts.filter((lift) => lift.currFloor === floor)"
         :key="lift.id"
         :style="{ position: 'absolute', left: 70 + (lift.id - 1) * 120 + 'px' }"
         class="lift-container"
       >
-        <LiftEmulatorLift />
+        <LiftEmulatorLift :lift="lift" />
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.floors {
-  display: flex;
-  flex-direction: column-reverse;
-}
-
-.floor {
-  width: 100vw;
-  height: 110px;
-  padding: 10px 10px 0 10px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  border-bottom: 1px solid #111;
-}
-
-.floor_btn {
-  margin-right: 20px;
-  width: 30px;
-  height: 30px;
-  border: none;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 18px;
-  font-weight: 700;
-  color: #111;
-  background: #03a1fc;
-}
+@import url(../assets/floors.css);
 </style>
