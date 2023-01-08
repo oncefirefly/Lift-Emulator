aecb5740382cedb2d45407a8ffb83dec655d5918
oncefirefly <oncefirefly@gmail.com>
2023-01-08 20:32:07 +0300

    Lift aminations & destFloor indicator added

--- src/components/LiftEmulatorLift.vue
+++ src/components/LiftEmulatorLift.vue
@@ -1,16 +1,68 @@
 <script>
-export default {};
+export default {
+  props: {
+    lift: {
+      type: Object,
+      required: true,
+      default: null,
+    },
+  },
+
+  computed: {
+    moveLiftStyle() {
+      if (
+        this.lift.destFloor > 0 &&
+        this.lift.distance > 0 &&
+        this.lift.busy &&
+        !this.lift.resting
+      ) {
+        return this.lift.destFloor > this.lift.currFloor
+          ? {
+              transform: `translateY(${this.lift.distance * 100 * -1}%)`,
+              transition: `transform ${
+                1000 * this.lift.distance
+              }ms ease-in-out`,
+            }
+          : {
+              transform: `translateY(${this.lift.distance * 100}%)`,
+              transition: `transform ${
+                1000 * this.lift.distance
+              }ms ease-in-out`,
+            };
+      }
+
+      return {};
+    },
+
+    rotateArrow() {
+      return this.lift.destFloor > this.lift.currFloor
+        ? {
+            transform: "rotate(180deg)",
+          }
+        : {};
+    },
+  },
+};
 </script>
 
 <template>
-  <div class="lift"></div>
+  <div
+    class="lift"
+    :class="{ resting: this.lift.resting }"
+    :style="moveLiftStyle"
+  >
+    <div v-if="lift.busy && !lift.resting" class="lift-distance-pointer">
+      {{ lift.destFloor }}
+      <img
+        class="lift-pointer-arrow"
+        src="../assets/img/arrow.svg"
+        alt="arrow"
+        :style="rotateArrow"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.lift {
-  border: 1px solid #111;
-  width: 100px;
-  height: 100px;
-  background: #fcba03;
-}
+@import url(../assets/lift.css);
 </style>
